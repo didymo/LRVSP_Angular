13c78eef45c334dea664c4944505571ecd916058
Benjamin Herne <2001herne@gmail.com>
2024-09-04 15:20:44 +1000

    Updated to use Angular-stle <fa-icon/> copmponent over <i class="fa"> syntax

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons/faSearch";
+import {faCog} from "@fortawesome/free-solid-svg-icons/faCog";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   imports: [
-    RouterLink
+    RouterLink,
+    FaIconComponent
   ],
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent {
+  protected readonly faSearch = faSearch;
+  protected readonly faCog = faCog;
+}
