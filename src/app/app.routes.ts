1d1849c40ab7e9ea99ebc7f75c6d0aa5a94ce30e
aub908 <141598518+aub908@users.noreply.github.com>
2024-09-12 16:34:12 +1000

    updated dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {SupportComponent} from "./support/support.component";
 import {TreeDataManagerComponent} from "./app/tree-data-manager/tree-data-manager.component";
+import {FeatureComponent} from "./feature/feature.component";
+import {SettingComponent} from "./setting/setting.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'login', component: LoginComponent},
   {path: 'support', component: SupportComponent},
+  {path: 'feature', component: FeatureComponent},
+  {path: 'setting', component: SettingComponent},
   {path: 'app', component: TreeDataManagerComponent},
 
 ];
