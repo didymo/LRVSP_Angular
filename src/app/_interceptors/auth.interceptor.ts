a7b03865223dfb8ef1a1efab279a2a10d57a210b
Benjamin Herne <2001herne@gmail.com>
2024-10-02 00:08:36 +1000

    Update angular.json, home.component.html, home.component.ts, and 18 more files

---
+++ src/app/_interceptors/auth.interceptor.ts
@@ -0,0 +1,55 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {AuthService} from "../_services/auth.service";
+import {catchError, switchMap, throwError} from "rxjs";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const authToken = authService.getAuthTokenFromStorage();
+
+  if (authToken) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authToken}`
+      }
+    });
+  }
+
+  return next(req).pipe(
+    catchError(error => {
+      console.log('Caught error:', error); // Log the error
+
+      // Temporary workaround - backend return 403
+      if (error.status === 401 || error.status === 403) {
+        console.log('401 error detected, attempting to refresh token'); // Log the detection of a 401 error
+
+        return authService.refreshTokenMethod().pipe(
+          switchMap((token: any) => {
+            console.log('Token refreshed:', token); // Log the new token
+
+            authService.setTokens(token.access_token, token.refresh_token);
+            if (authToken && !req.headers.has('Authorization')) {
+              req = req.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${authToken}`
+                }
+              });
+            }
+
+            return next(req);  // Ensure an Observable is returned here
+          }),
+          catchError(err => {
+            console.log('Token refresh failed, logging out', err); // Log the refresh failure
+            //We only want to log out if we're not logged in - browsing the home page while not logged in is valid
+            // if (authService.isLoggedIn()) {
+            authService.logout();
+            // }
+            return throwError(err);
+          })
+        );
+      }
+
+      return throwError(error);
+    })
+  );
+};
--- src/app/_interceptors/cache-interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CacheInterceptor } from './interceptors';
-
-describe('CacheInterceptor', () => {
-  it('should create an instance', () => {
-    expect(new CacheInterceptor()).toBeTruthy();
-  });
-});
